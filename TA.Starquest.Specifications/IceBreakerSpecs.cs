45e38b9199be77bbc37160a2edcb87a73420e1df
Tim Long <Tim@tigranetworks.co.uk>
2020-08-10 20:45:59 +0100

    Made the ice-breaker test actually exercise some code.

--- TA.Starquest.Specifications/IceBreakerSpecs.cs
+++ TA.Starquest.Specifications/IceBreakerSpecs.cs
@@ -1,6 +1,12 @@
-﻿using Machine.Specifications;
+﻿using System.Linq;
+using Machine.Specifications;
+using TA.Starquest.Core;
+using TA.Utils.Core;
 
-internal class when_running_tests
+internal class when_creating_a_picklist_item
     {
-    It should_show_test_results_in_the_test_runner = () => { };
+    Because of = () => result = new PickListItem<int>(1,"Test Item");
+    It should_set_the_display_name = () => result.DisplayName.ShouldEqual("Test Item");
+    It should_set_the_key = () => result.Id.ShouldEqual(1);
+    private static PickListItem<int> result;
     }
