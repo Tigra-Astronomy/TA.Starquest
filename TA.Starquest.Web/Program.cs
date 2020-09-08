fb647d9a9b2adfb4595c6f794f7d95b09e60ea89
Tim Long <Tim@tigranetworks.co.uk>
2020-09-08 16:48:31 +0100

    Run DB migrations before starting host

--- TA.Starquest.Web/Program.cs
+++ TA.Starquest.Web/Program.cs
@@ -3,18 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using TA.Starquest.DataAccess.EFCore;
 
 namespace TA.Starquest.Web
-    {
+{
     public class Program
-        {
+    {
         public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
             {
-            CreateHostBuilder(args).Build().Run();
+                var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                db.Database.Migrate();
             }
+            host.Run();
+        }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
@@ -22,5 +31,5 @@ namespace TA.Starquest.Web
                 {
                     webBuilder.UseStartup<Startup>();
                 });
-        }
     }
+}
